4820aa1499ded169bd8a9534e682102bf6becac1
Soomin Choi <choisium@gmail.com>
2021-09-12 20:19:26 +0900

    assn1: add solution codes for assn1 problems

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,39 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || r == 0 || c == r) 1 else pascal(c-1, r-1) + pascal(c, r-1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+
+  def balance(chars: List[Char]): Boolean = {
+    def getCount(count: Int, ch: Char) = {
+      if (ch == '(') count + 1
+      else if (ch == ')') count - 1
+      else count
+    }
+
+    def checkBalance(count: Int, chars: List[Char]): Boolean = {
+      // println(count, chars.head)
+      if (count < 0) false
+      else if (chars.isEmpty) count == 0
+      else checkBalance(getCount(count, chars.head), chars.tail)
+    }
+
+    checkBalance(0, chars)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (coins.isEmpty) 0
+    else if (money < coins.head) countChange(money, coins.tail)
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+
+  }
 }

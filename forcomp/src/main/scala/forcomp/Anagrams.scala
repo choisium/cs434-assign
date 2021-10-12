9ded4fe9610d660a31105e9b73faa2352da883c1
Soomin Choi <choisium@gmail.com>
2021-10-12 15:23:35 +0900

    assn5: add solution codes for assn5 problem

--- forcomp/src/main/scala/forcomp/Anagrams.scala
+++ forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -33,10 +33,12 @@ object Anagrams {
    *  Note: the uppercase and lowercase version of the character are treated as the
    *  same character, and are represented as a lowercase character in the occurrence list.
    */
-  def wordOccurrences(w: Word): Occurrences = ???
+  def wordOccurrences(w: Word): Occurrences = {
+    w.toLowerCase.groupBy((c: Char) => c).map{case (c, cs) => (c -> cs.length)}.toList.sorted
+  }
 
   /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences = wordOccurrences(s mkString "")
 
   /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
    *  the words that have that occurrence count.
@@ -53,10 +55,12 @@ object Anagrams {
    *    List(('a', 1), ('e', 1), ('t', 1)) -> Seq("ate", "eat", "tea")
    *
    */
-  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = ???
+  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = {
+    dictionary.groupBy((w: Word) => wordOccurrences(w)) withDefaultValue List()
+  }
 
   /** Returns all the anagrams of a given word. */
-  def wordAnagrams(word: Word): List[Word] = ???
+  def wordAnagrams(word: Word): List[Word] = dictionaryByOccurrences(wordOccurrences(word))
 
   /** Returns the list of all subsets of the occurrence list.
    *  This includes the occurrence itself, i.e. `List(('k', 1), ('o', 1))`
@@ -80,7 +84,21 @@ object Anagrams {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def combinations(occurrences: Occurrences): List[Occurrences] = {
+    // I used occurrences.reverse to make the result is sorted
+    ((occurrences.reverse foldLeft (Set(List()): Set[Occurrences])) {
+      (acc: Set[Occurrences], element: (Char, Int)) => {
+        val (char, n) = element
+        (for {
+          i <- 0 to n
+          occurrence <- acc
+        } yield {
+          if (i != 0) ((char, i) :: occurrence)
+          else occurrence
+        }).toSet
+      }
+    }).toList
+  }
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    * 
@@ -92,7 +110,18 @@ object Anagrams {
    *  Note: the resulting value is an occurrence - meaning it is sorted
    *  and has no zero-entries.
    */
-  def subtract(x: Occurrences, y: Occurrences): Occurrences = ???
+  def subtract(x: Occurrences, y: Occurrences): Occurrences = {
+    (y foldLeft (x.toMap withDefaultValue 0)) {
+      (xMap, occurY) => {
+        val (charY, numY) = occurY
+
+        assert(xMap(charY) != 0)
+
+        if (xMap(charY) == numY) xMap - charY
+        else xMap updated(charY, xMap(charY) - numY)
+      }
+    }.toList.sorted
+  }
 
   /** Returns a list of all anagram sentences of the given sentence.
    *  
@@ -134,6 +163,23 @@ object Anagrams {
    *
    *  Note: There is only one anagram of an empty sentence.
    */
-  def sentenceAnagrams(sentence: Sentence): List[Sentence] = ???
+  def sentenceAnagrams(sentence: Sentence): List[Sentence] = {
+    def occurrencesAnagrams(occurrences: Occurrences): List[Sentence] = {
+      if (occurrences.isEmpty) List(List())
+      else {
+        val combs = combinations(occurrences)
+        val wordsAndRestOccurrences = for (comb <- combs) yield (dictionaryByOccurrences(comb), subtract(occurrences, comb))
+        for {
+          (words, restOccurrences) <- wordsAndRestOccurrences
+          word <- words
+          restSentence <- occurrencesAnagrams(restOccurrences)
+        } yield {
+          word :: restSentence
+        }
+      }
+    }
 
+    val occurs = sentenceOccurrences(sentence)
+    occurrencesAnagrams(occurs)
+  }
 }
